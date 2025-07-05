5ca0db498d37829da81ebbbc4e9d23dcf4bcfd71
HendSayed25 <hendsayed853@gmail.com>
2025-07-05 19:21:02 +0300

    add colors and font

--- app/src/main/java/com/example/caffeine/ui/theme/Color.kt
+++ app/src/main/java/com/example/caffeine/ui/theme/Color.kt
@@ -14,8 +14,17 @@ val white = Color(0xFFFFFFFF)
 val lightWhite = Color(0xDEFFFFFF)
 val black = Color(0xFF030004)
 val brawn = Color(0xFF291710)
-val caffe = Color(0xFF7C351B)
+val coffeeSizeColor = Color(0xFF7C351B)
 val lightBeach = Color(0xFFFFEEE7)
 val LightGrayBackground = Color(0xFFF5F5F5)
 val fontColor = Color(0xDE1F1F1F)
 val shadowColor = Color(0x241F1F1F)
+val titleColor = Color(0xFFB3B3B3)
+val subTitleColor = Color(0xFF3B3B3B)
+val subTitleColor2 = Color(0xCC1F1F1F)
+val coffeeSizeSelectedTxtColor = Color(0xDEFFFFFF)
+val coffeeSizeUnSelectedTxtColor = Color(0x991F1F1F)
+val coffeeSizeShadow = Color(0x80B94B23)
+
+
+
