501ec638c4f5d98391d001d24fa2354588bb6cdd
HendSayed25 <hendsayed853@gmail.com>
2025-07-17 15:18:12 +0300

    Refactor TopSection composable to use Box

--- app/src/main/java/com/example/caffeine/screens/prepareCoffee/composable/TopSection.kt
+++ app/src/main/java/com/example/caffeine/screens/prepareCoffee/composable/TopSection.kt
@@ -1,7 +1,7 @@
 package com.example.caffeine.screens.prepareCoffee.composable
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
@@ -13,29 +13,37 @@ import androidx.compose.ui.unit.dp
 import com.example.caffeine.R
 import com.example.caffeine.composable.AppBar
 
+
 @Composable
 fun TopSection(
     modifier: Modifier = Modifier,
-    onClickBack :() -> Unit,
-    widthOfCup : Int,
-){
-    Column (
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    onClickBack: () -> Unit,
+    widthOfCup: Int,
+) {
+    Box(modifier = modifier) {
+        // AppBar
         AppBar(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp),
             onClickBack = onClickBack
         )
 
+
         DoneCoffeeTopSection(
-            modifier = Modifier.padding(top=16.dp)
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 72.dp)
         )
+
 
         Image(
             painter = painterResource(R.drawable.cover_of_cup),
             contentDescription = "cover of the cup",
-            modifier = Modifier.padding(top=17.dp).width(widthOfCup.dp)
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 220.dp)
+                .width(widthOfCup.dp)
         )
     }
 }
